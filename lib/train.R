fa2fdb77666315e29c18f4997af9fc76467b103e
Dennis Shpits <9089654+dennisshpits@users.noreply.github.com>
2021-03-10 21:24:23 -0500

    Add initial baseline model (#3)

--- lib/train.R
+++ lib/train.R
@@ -6,3 +6,32 @@ train <- function(features, labels, w = NULL, l = 1){
   model <- glmnet(features, labels, weights = w, alpha = 1, family = "binomial", lambda = l)
   return(model)
 }
+
+train_gbm <- function(features, s, K = 5, n=100, w = NULL){
+  
+  if (!is.null(w)){
+    model<- gbm(label~. ,data = features,
+                distribution = "multinomial", 
+                n.trees = n,
+                shrinkage = s,
+                n.minobsinnode = 10, 
+                cv.folds = K,
+                weights = w,
+                verbose = TRUE)
+    
+  } else {
+    features$label <- as.factor(features$label)
+    trainSplit <- SMOTE(label ~ ., features, perc.over = 100, perc.under=200)
+    
+    model<- gbm(label~. ,data = trainSplit,
+                distribution = "multinomial", 
+                n.trees = n,
+                shrinkage = s,
+                n.minobsinnode = 10, 
+                cv.folds = K,
+                verbose = TRUE)
+  }
+  
+  
+  return(model)
+}
