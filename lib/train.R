0974f085767d33c6a089e08bcda667ae3e259f05
Yingyao Wu <62625497+kattywu@users.noreply.github.com>
2021-03-17 12:02:14 -0400

    Cleaned up main.Rmd, knitted pdf will be updated later

--- lib/train.R
+++ lib/train.R
@@ -2,16 +2,80 @@
 ### Train a classification model with training features ###
 ###########################################################
 
+## starter code
 train <- function(features, labels, w = NULL, l = 1){
   model <- glmnet(features, labels, weights = w, alpha = 1, family = "binomial", lambda = l)
   return(model)
 }
 
+## Random Forest
+## old features
+if(!require("randomForest")){
+  install.packages("randomForest")
+}
+library(randomForest)
+
+random_forest_tune <- function(df){
+  set.seed(2020)
+  x = as.matrix(df[, -6007])
+  y = factor(df[, 6007])
+  return(
+    tuneRF(x = x,
+           y = y,
+           ntreeTry = 1000,
+           improve = 0.05,
+           stepFactor = 2)
+  )
+}
+old_random_forest_train <- function(df, mtry, tree_number, node_size){
+  x = as.matrix(df[, -6007])
+  y = df[, 6007]
+  y = as.character(y)
+  y = as.factor(y)
+  set.seed(2020)
+  return(
+    randomForest(x = x,
+                 y = y,
+                 mtry = mtry,
+                 ntree = tree_number,
+                 sampsize = nrow(x),
+                 nodesize = node_size
+    )
+  )
+}
+
+## new features
+random_forest_tune <- function(df){
+  set.seed(2020)
+  x = as.matrix(df[, -6023])
+  y = factor(df[, 6023])
+  return(
+    tuneRF(x = x,
+           y = y,
+           ntreeTry = 1500,
+           improve = 0.05,
+           stepFactor = 2)
+  )
+}
+random_forest_train <- function(df, mtry, tree_number, node_size){
+  x = as.matrix(df[, -6023])
+  y = df[, 6023]
+  y = as.character(y)
+  y = as.factor(y)
+  set.seed(2020)
+  return(
+    randomForest(x = x,
+                 y = y,
+                 mtry = mtry,
+                 ntree = tree_number,
+                 sampsize = nrow(x),
+                 nodesize = node_size
+    )
+  )
+}
+
+
 ## SVM
-# svm_default_train = function(training_data, cv){
-#   svm.fit <- svm(label ~ ., data = training_data, type = "C-classification", cross = cv)
-#   return(svm.fit)
-# }
 
 svm_linear_cost_tune <- function(training_data){
   best_cost <- tune(method = svm,
@@ -77,7 +141,7 @@ train_gbm <- function(features, s, K = 5, n=100, w = NULL){
   return(model)
 }
 
-# ridge
+## Ridge
 ridge_train <- function(train_data, alpha, K, lambda){
   
   ### Input: a data frame containing features and labels and a parameter list.
