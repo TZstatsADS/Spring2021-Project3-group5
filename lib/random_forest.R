567aff6e82f8e424f40f9c808fb589029aa871a1
Jingbin Cao <66717862+JingbinCao@users.noreply.github.com>
2021-03-16 22:36:29 -0400

    Random forest update 1.0 (#18)
    * Random Forest Model with New Feature Finished
    
    * Final version 1.0

--- lib/random_forest.R
+++ lib/random_forest.R
@@ -3,124 +3,37 @@ if(!require("randomForest")){
 }
 library(randomForest)
 random_forest_tune <- function(df){
-  set.seed(0)
-  x = as.matrix(df[, -6007])
-  y = factor(df[, 6007])
+  set.seed(2020)
+  x = as.matrix(df[, -6023])
+  y = factor(df[, 6023])
   return(
     tuneRF(x = x,
            y = y,
-           ntreeTry = 500,
+           ntreeTry = 1500,
            improve = 0.05,
            stepFactor = 2)
   )
 }
-random_forest_train <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
+random_forest_train <- function(df, mtry, tree_number, node_size){
+  x = as.matrix(df[, -6023])
+  y = df[, 6023]
   y = as.character(y)
   y = as.factor(y)
-  set.seed(0)
+  set.seed(2020)
   return(
     randomForest(x = x,
                  y = y,
                  mtry = mtry,
-                 ntree = 1000,
+                 ntree = tree_number,
                  sampsize = nrow(x),
-                 nodesize = 25
+                 nodesize = node_size
     )
   )
 }
 random_forest_test <- function(model, testset){
-  test.x = as.matrix(testset[ , -6007])
-  set.seed(0)
+  test.x = as.matrix(testset[ , -6023])
+  set.seed(2020)
   return(
     predict(model, test.x)
-  )
-}
-
-
-########## Test different tree size
-random_forest_train_500 <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
-  y = as.character(y)
-  y = as.factor(y)
-  set.seed(0)
-  return(
-    randomForest(x = x,
-                 y = y,
-                 mtry = mtry,
-                 ntree = 500,
-                 sampsize = nrow(x),
-                 nodesize = 25
-    )
-  )
-}
-
-random_forest_train_1000 <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
-  y = as.character(y)
-  y = as.factor(y)
-  set.seed(0)
-  return(
-    randomForest(x = x,
-                 y = y,
-                 mtry = mtry,
-                 ntree = 1000,
-                 sampsize = nrow(x),
-                 nodesize = 25
-    )
-  )
-}
-
-random_forest_train_1500 <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
-  y = as.character(y)
-  y = as.factor(y)
-  set.seed(0)
-  return(
-    randomForest(x = x,
-                 y = y,
-                 mtry = mtry,
-                 ntree = 1500,
-                 sampsize = nrow(x),
-                 nodesize = 25
-    )
-  )
-}
-
-random_forest_train_2000 <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
-  y = as.character(y)
-  y = as.factor(y)
-  set.seed(0)
-  return(
-    randomForest(x = x,
-                 y = y,
-                 mtry = mtry,
-                 ntree = 2000,
-                 sampsize = nrow(x),
-                 nodesize = 25
-    )
-  )
-}
-
-random_forest_train_2500 <- function(df, mtry){
-  x = as.matrix(df[, -6007])
-  y = df[, 6007]
-  y = as.character(y)
-  y = as.factor(y)
-  set.seed(0)
-  return(
-    randomForest(x = x,
-                 y = y,
-                 mtry = mtry,
-                 ntree = 2500,
-                 sampsize = nrow(x),
-                 nodesize = 25
-    )
   )
 }
